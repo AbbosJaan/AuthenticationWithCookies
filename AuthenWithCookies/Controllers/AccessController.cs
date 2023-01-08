3097151abf654c4233b47271dee5aeac62c61c17
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-01-08 18:41:26 +0500

    Add project files.

---
+++ AuthenWithCookies/Controllers/AccessController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using AuthenWithCookies.Models;
+
+namespace AuthenWithCookies.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if (claimUser.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(VMLogin modelLogin)
+        {
+            if(modelLogin.Email == "Admin@site.com" && modelLogin.Password == "123")
+            {
+                List<Claim> claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Email),
+                    new Claim("Other properties", "Exampe: roles"),
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = modelLogin.KeepLogedIn,
+
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity), properties);
+                return RedirectToAction("Index", "Home");
+            }
+            ViewData["ValidateMessage"] = "user not found";
+            return View();
+        }
+    }
+}
